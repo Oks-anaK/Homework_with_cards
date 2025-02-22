501bc08d319c3bc68d31eb5bd9b2825d2667bd84
Oksana Konkina <oksanka.808@mail.ru>
2025-02-22 03:59:43 +0300

    final version 17.02.25 (edit2

--- src/widget.py
+++ src/widget.py
@@ -6,10 +6,19 @@ def mask_account_card(_data: str) -> str:
     Функция переиспользует уже существующие функции маскировки из модуля masks
     и возвращает строку с замаскированным номером
     """
+    if not isinstance(_data, str):
+        raise TypeError("Неверный тип данных.")
     splitted_data = _data.split(" ")
     digits_only = splitted_data[-1]
     name_of_card_or_account = splitted_data[:-1]
-    if "Счет" in _data:
+    if _data.isdigit() is True:
+        if len(_data) == 20:
+            masked_digits_only = get_mask_account(digits_only)
+            return masked_digits_only
+        else:
+            masked_digits_only = get_mask_card_number(digits_only)
+            return masked_digits_only
+    elif "Счет" in _data:
         masked_digits_only = get_mask_account(digits_only)
     else:
         masked_digits_only = get_mask_card_number(digits_only)
@@ -21,13 +30,38 @@ def get_date(_date: str) -> str:
     Функция принимает на вход строку с датой в формате "2024-03-11T02:26:18.671407"
     и возвращает строку с датой в формате "ДД.ММ.ГГГГ"
     """
-    return f"{_date[8:10]}.{_date[5:7]}.{_date[0:4]}"
+    if not isinstance(_date, str):
+        raise TypeError("Неверный тип данных.")
+    elif not _date:
+        return "Пустая строка."
+
+    year = int(_date[0:4])
+    month = int(_date[5:7])
+    day = int(_date[8:10])
+
+    if not (1 <= month <= 12 and 1 <= day <= 31):  # Проверка числа и месяца
+        return "Некорректный ввод."
+    # Проверка високосного года (не учитывает все нюансы)
+    if month == 2 and day > 29:
+        return "Некорректный ввод."
+    # Проверка дней в месяцах
+    elif month in [4, 6, 9, 11] and day == 31:
+        return "Некорректный ввод."
+    # `d`:  Вывести число как десятичное.
+    #  `02`:  Вывести число с минимальной шириной 2 символа,
+    # дополняя слева нулями, если число меньше двух цифр.
+    return f"{day:02d}.{month:02d}.{year}"
 
 
-if __name__ == "__main__":
-    print(mask_account_card("Visa Platinum 7000792289606361"))
-    print(mask_account_card("Счет 73654108430135874305"))
-    print(get_date("2024-03-11T02:26:18.671407"))
-    print(mask_account_card("MasterCard 7158300734726758"))
-    print(mask_account_card("Счет 35383033474447895560"))
-    print(mask_account_card("Maestro 1596837868705199"))
+# if __name__ == "__main__":
+#     print(mask_account_card("Visa Platinum 7000792289606361"))
+#     print(mask_account_card("Счет 73654108430135874305"))
+#     print(get_date("2024-03-11T02:26:18.671407"))
+#     print(mask_account_card("MasterCard 7158300734726758"))
+#     print(mask_account_card("Счет 35383033474447895560"))
+#     print(mask_account_card("Maestro 1596837868705199"))
+#     print(mask_account_card("485395894859485948"))
+#     print(mask_account_card("53894584093049688999"))
+#     print(mask_account_card(""))
+#     print(get_date("2022/13/32"))
+#     print(get_date("2000.02.30"))
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,9 @@
 import pytest
 
 
+@pytest.fixture
 # Фикстура, которая используется в качестве входных данных для функций:
 # test_filter_by_state_normal, test_sort_by_date_normal_1
-@pytest.fixture
 def trans_date() -> list[dict]:
     return [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
@@ -13,9 +13,9 @@ def trans_date() -> list[dict]:
     ]
 
 
+@pytest.fixture
 # Фикстура, которая используется в качестве входных данных для функции:
 # test_sort_by_date_normal_2
-@pytest.fixture
 def same_data_trans() -> list[dict]:
     return [
         {"id": 939719570, "state": "EXECUTED", "date": "2016-09-12T02:08:58.425572"},
